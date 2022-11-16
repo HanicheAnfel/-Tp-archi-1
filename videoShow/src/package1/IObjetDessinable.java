86ded70de57aacfd260d2ca191c5f30af628b6dc
Haniche Anfel <henicheanfel@gmail.com>
2022-11-16 23:06:25 +0100

    réponse de 1 à 9

---
+++ videoShow/src/package1/IObjetDessinable.java
@@ -0,0 +1,17 @@
+package package1;
+
+import java.awt.Graphics;
+
+public interface IObjetDessinable {
+public void dessiner(Graphics g);
+
+void deplacer();
+/**
+ * Fait effectuer au visage un déplacement élementaire. La position du coin
+ * supérieur gauche du visage est modifiée en lui ajoutant le déplacement
+ * élémentaire défini par dx et dy. Si le visage dépasse de l'un des bords
+ * de la zone de dessin il inverse sa direction de déplacement.
+ */
+
+
+}
